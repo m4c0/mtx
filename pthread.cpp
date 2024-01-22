b39950106fb987e881cad8a5ffd9ab62c7c44093
Eduardo Costa <m4c0@users.noreply.github.com>
2024-01-22 12:50:14 +0000

    pthread: conditional variables

--- pthread.cpp
+++ pthread.cpp
@@ -20,3 +20,25 @@ mtx::lock::lock(mtx::mutex *m) : m_mutex{m} {
   pthread_mutex_lock(&*(m_mutex->m_handle));
 }
 mtx::lock::~lock() { pthread_mutex_unlock(&*(m_mutex->m_handle)); }
+
+struct mtx::cond::pimpl : pthread_cond_t {};
+
+mtx::cond::cond() : m_handle{new mtx::cond::pimpl{}} {
+  pthread_cond_init(&*m_handle, nullptr);
+}
+mtx::cond::~cond() {
+  if (m_handle)
+    pthread_cond_destroy(&*m_handle);
+}
+
+mtx::cond::cond(cond &&) = default;
+mtx::cond &mtx::cond::operator=(cond &&) = default;
+
+void mtx::cond::wait(lock *l) {
+  timespec ts{};
+  clock_gettime(CLOCK_REALTIME, &ts);
+  ts.tv_nsec += 100 * 1000 * 1000;
+  pthread_cond_timedwait(&*m_handle, &*l->m_mutex->m_handle, &ts);
+}
+void mtx::cond::wake_one() { pthread_cond_signal(&*m_handle); }
+void mtx::cond::wake_all() { pthread_cond_broadcast(&*m_handle); }
