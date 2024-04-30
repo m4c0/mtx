dc23ad185bbdcfd3853c5578518ef16798a2e08c
Eduardo Costa <m4c0@users.noreply.github.com>
2024-04-30 20:53:02 +0100

    fix build on linux

--- pthread.cpp
+++ pthread.cpp
@@ -3,32 +3,36 @@ module;
 
 module mtx;
 
-struct mtx::mutex::pimpl : pthread_mutex_t {};
+struct mtx::mutex::pimpl {
+  pthread_mutex_t m;
+};
 
 mtx::mutex::mutex() : m_handle{new mtx::mutex::pimpl{}} {
-  pthread_mutex_init(&*m_handle, nullptr);
+  pthread_mutex_init(&m_handle->m, nullptr);
 }
 mtx::mutex::~mutex() {
   if (m_handle)
-    pthread_mutex_destroy(&*m_handle);
+    pthread_mutex_destroy(&m_handle->m);
 }
 
 mtx::mutex::mutex(mutex &&) = default;
 mtx::mutex &mtx::mutex::operator=(mutex &&) = default;
 
 mtx::lock::lock(mtx::mutex *m) : m_mutex{m} {
-  pthread_mutex_lock(&*(m_mutex->m_handle));
+  pthread_mutex_lock(&m_mutex->m_handle->m);
 }
-mtx::lock::~lock() { pthread_mutex_unlock(&*(m_mutex->m_handle)); }
+mtx::lock::~lock() { pthread_mutex_unlock(&m_mutex->m_handle->m); }
 
-struct mtx::cond::pimpl : pthread_cond_t {};
+struct mtx::cond::pimpl {
+  pthread_cond_t p;
+};
 
 mtx::cond::cond() : m_handle{new mtx::cond::pimpl{}} {
-  pthread_cond_init(&*m_handle, nullptr);
+  pthread_cond_init(&m_handle->p, nullptr);
 }
 mtx::cond::~cond() {
   if (m_handle)
-    pthread_cond_destroy(&*m_handle);
+    pthread_cond_destroy(&m_handle->p);
 }
 
 mtx::cond::cond(cond &&) = default;
@@ -38,7 +42,7 @@ void mtx::cond::wait(lock *l) {
   timespec ts{};
   clock_gettime(CLOCK_REALTIME, &ts);
   ts.tv_nsec += 100 * 1000 * 1000;
-  pthread_cond_timedwait(&*m_handle, &*l->m_mutex->m_handle, &ts);
+  pthread_cond_timedwait(&m_handle->p, &l->m_mutex->m_handle->m, &ts);
 }
-void mtx::cond::wake_one() { pthread_cond_signal(&*m_handle); }
-void mtx::cond::wake_all() { pthread_cond_broadcast(&*m_handle); }
+void mtx::cond::wake_one() { pthread_cond_signal(&m_handle->p); }
+void mtx::cond::wake_all() { pthread_cond_broadcast(&m_handle->p); }
